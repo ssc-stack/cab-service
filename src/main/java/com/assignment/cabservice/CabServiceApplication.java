8af634e2fd60f37a637f31a49e54f9041103611b
ssc-stack <shashanktech2@gmail.com>
2022-12-08 14:01:42 +0530

    First Commit after project creation

---
+++ src/main/java/com/assignment/cabservice/CabServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.cabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/cabservice/CabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.cabservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
